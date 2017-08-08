6cc596e298e8821fe663b6d0fef2ef650e9b8375
baleen37 <baleen37@gmail.com>
2017-08-09 01:52:22 +0900

    fix bug

--- sample/src/main/java/me/baleen/sharedpreferencedelegate/UserPreference.kt
+++ sample/src/main/java/me/baleen/sharedpreferencedelegate/UserPreference.kt
@@ -4,7 +4,7 @@ package me.baleen.sharedpreferencedelegate
  * Created by baleen37 on 09/08/2017.
  */
 class UserPreference : PreferenceUtil() {
-    var email by StringPref("unknown")
+    var email by StringPref(defaultValue = "unknown")
     var age by IntPref("PREF_AGE")
     var male by BooleanPref(defaultValue = false)
     var score by FloatPref()
